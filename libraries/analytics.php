c2c92c8b05f3870390409426a6a07cc1478c1805
dexterns88 <dexter_ns88@hotmail.com>
2013-05-24 00:31:08 +0200

    create spark google analitics

---
+++ libraries/analytics.php
@@ -0,0 +1,34 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); 
+
+class Analytics {
+	
+	private $ci;    // for CodeIgniter Super Global Reference.
+	private $path;
+	
+	public function __construct()
+	{
+		$this->ci =& get_instance();
+		$this->ci->config->load('config');
+		$this-> path = $this->ci->config->item('source');
+		$this->analitic();
+  }
+  
+  private function analitic()
+  {
+		if (defined('ENVIRONMENT') )
+		{
+			if( ENVIRONMENT != 'development' )
+			{
+				$analitic = $this->path;
+				$script = "<script type='text/javascript' src='{$analitic}'></script>";
+				define('ganalitics', $script );
+			}
+		}
+		else
+		{
+			show_error("ENVIRONMENT undefined");
+		}
+		
+	}
+  
+}
